5bc1230e859b24278a4574771c0ada805b866b87
FABIEN VIAL <vialf@ad.u-ga.fr>
2021-10-22 17:32:18 +0200

    AccesSequentiel Mtd: Avancer, Elementcourant,Constructeur

--- src/main/java/inf353/LecteurDocumentNaif.java
+++ src/main/java/inf353/LecteurDocumentNaif.java
@@ -5,6 +5,35 @@ public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
     /**
      * Initialisation du parcours.
      */
+    public int nbSep = 17; // nombre de separateurs
+    public char[] separateur; // liste des separateurs
+    public int i = 0; // indice du caractere dans le texte
+    public String mot;
+    public BufferedReader br
+
+    public LecteurDocumentNaif(File file)
+    {
+        br = new BufferedReader(new FileReader(file));
+        separateur = new char[nbSep];
+        separateur[0] = ',';
+        separateur[1] = '?';
+        separateur[2] = '.';
+        separateur[4] = ';';
+        separateur[5] = ':';
+        separateur[6] = '/';
+        separateur[7] = '!';
+        separateur[8] = ' ';
+        separateur[9] = '\n'; // retour chariot
+        separateur[10] = '(';
+        separateur[11] = ')';
+        separateur[12] = '\"';
+        separateur[13] = '-';
+        separateur[14] = '\'';// apostrophe
+        separateur[15] = '[';
+        separateur[16] = ']';
+
+    }
+
     public void demarrer() {
 
     }
@@ -13,7 +42,18 @@ public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
      * Passage à l'élément suivant
      */
     public void avancer() {
-
+        if(!finDeSequence())
+        {
+            while(!finDeSequence() && !estSeparateur(mot.charAt(i)))
+            {
+                i++;
+            }
+            if(!finDeSequence())
+            {
+                i++;
+            }
+            
+        }
     }
 
     /**
@@ -29,7 +69,28 @@ public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
      * @return
      */
     public String elementCourant() {
-        return "";
+        String res = "";
+        int j = i;
+        if(!finDeSequence())
+        {
+            while(!finDeSequence() && !estSeparateur(mot.charAt(j)))
+            {
+                res = res + mot.charAt(j);
+                j++;
+            }
+            
+        }
+        return res;
     }
 
+
+    public boolean estSeparateur(char c) //Renvoie vrai si le caractères est separateur
+    {
+        int j = 0;
+        while (j < nbSep && separateur[j] != c)
+        {
+            j++;
+        }
+        return j < nbSep;
+    }
 }
