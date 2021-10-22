fa379c775237cdd771e9161a015b949eb68d732e
ValuWaurld <val0u74mc@gmail.com>
2021-10-22 16:36:38 +0200

    Initialisation des classes principales

---
+++ src/main/java/inf353/LecteurDocumentNaif.java
@@ -0,0 +1,35 @@
+package inf353;
+
+public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
+    
+    /**
+     * Initialisation du parcours.
+     */
+    public void demarrer() {
+
+    }
+
+    /**
+     * Passage à l'élément suivant
+     */
+    public void avancer() {
+
+    }
+
+    /**
+     * vrai ssi la séquence est épuisée
+     * @return
+     */
+    public boolean finDeSequence() {
+        return true;
+    }
+
+    /**
+     * renvoie l'élément courant
+     * @return
+     */
+    public String elementCourant() {
+        return "";
+    }
+
+}
---
+++ src/main/java/inf353/MatriceIndexNaive.java
@@ -0,0 +1,53 @@
+package inf353;
+
+import java.io.FileNotFoundException;
+
+public class MatriceIndexNaive implements MatriceIndex {
+
+    int[][] matrice;
+
+    MatriceIndexNaive(int documents, int mots) {
+        this.matrice = new int[documents][mots];
+    }
+    
+    /**
+     * Sauvegarde de la matrice d'occurence dans le fichier nomDeFichier. Le format est libre,
+     * mais doit privilégier la vitesse de chargement et la compacité (taille du fichier).
+     *
+     * @param nomDeFichier
+     */
+    public void sauver(String nomDeFichier) throws FileNotFoundException {
+        
+    }
+
+    /**
+     * retourne le nombre d'occurences du terme numéro nterm dans le document numéro ndoc.
+     * @param  ndoc  le numéro du document
+     * @param  nterm le numéro du terme
+     * @return       le nombre d'occurences du terme dans le document
+     */
+    public int val(int ndoc, int nterm) {
+        return 0;
+    }
+
+    /**
+     * Ajoute 1 au nombre d'occurences du terme numéro nterm dans le document numéro ndoc.
+     * @param  ndoc  le numéro du document
+     * @param  nterm le numéro du terme
+     */
+    public void incremente(int ndoc, int nterm) {
+
+    }
+
+    /**
+     * affecte à val le numéro d'occurences du terme numéro nterm dans le document numéro ndoc.
+     * @param  ndoc  le numéro du document
+     * @param  nterm le numéro du terme
+     * @param val    la nouvelle valeur du nombre d'occurence
+     */
+    public void affecte(int ndoc, int nterm, int val) {
+
+    }
+
+    
+}
