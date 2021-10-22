38a567345573a3b845dac294491978eb0eda2ca0
ValuWaurld <val0u74mc@gmail.com>
2021-10-22 21:58:29 +0200

    Constructeurs de MatriceIndexNaive et tests

--- src/main/java/inf353/MatriceIndexNaive.java
+++ src/main/java/inf353/MatriceIndexNaive.java
@@ -2,19 +2,52 @@ package inf353;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Writer;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
 
 public class MatriceIndexNaive implements MatriceIndex {
 
-    int[][] matrice;
+    public int[][] matrice;
 
-    public MatriceIndexNaive(int documents, int mots) {
-        this.matrice = new int[documents][mots];
+    public MatriceIndexNaive(int docs, int termes) {
+        this.matrice = new int[docs][termes];
     }
 
-    public MatriceIndexNaive(File fichier) {
-
+    public MatriceIndexNaive(String nomFichier) throws FileNotFoundException, IOException {
+        File fichier = new File(nomFichier);
+        if (!fichier.exists() || !fichier.isFile()) {
+            throw new FileNotFoundException("Aucun fichier du nom de " + nomFichier + " n'a été trouvé.");
+        }
+        BufferedReader buffer = new BufferedReader(new FileReader(fichier));
+        String ligne = buffer.readLine();
+        int docs = 0;
+        int termes = 0;
+        while (ligne != null) {
+            docs = docs+1;
+            termes = Math.max(termes, ligne.split(",").length);
+            ligne = buffer.readLine();
+        }
+        buffer.close();
+        this.matrice = new int[docs][termes];
+        buffer = new BufferedReader(new FileReader(fichier));
+        ligne = buffer.readLine();
+        for (int i = 0; i < docs; i++) {
+            String[] tableau = ligne.split(",");
+            for (int j = 0; j < termes; j++) {
+                if (j < tableau.length) {
+                    this.affecte(i, j, Integer.valueOf(tableau[j]));
+                } else {
+                    this.affecte(i, j, 0);
+                }
+            }
+            ligne = buffer.readLine();
+        }
+        buffer.close();
     }
     
     /**
@@ -24,23 +57,32 @@ public class MatriceIndexNaive implements MatriceIndex {
      * @param nomDeFichier
      */
     public void sauver(String nomDeFichier) throws FileNotFoundException, IOException {
-        String contenu = "";
+        File fichier = new File(nomDeFichier);
+        if (fichier.exists() && fichier.isFile()) {
+            fichier.delete();
+        }
+        fichier.createNewFile();
+        Writer writer = new FileWriter(fichier, StandardCharsets.UTF_8);
+        BufferedWriter buffer = new BufferedWriter(writer);
+        String ligne = "";
         for (int mots = 0; mots < this.matrice[0].length; mots++) {
             for (int documents = 0; documents < this.matrice.length; documents++) {
-                contenu += this.matrice[mots][documents] + ",";
+                ligne += this.matrice[mots][documents] + ",";
             }
-            contenu += "\n";
+            ligne = ligne.substring(0, ligne.length()-1);
+            buffer.write(ligne);
+            buffer.newLine();
+            ligne = "";
         }
-        RandomAccessFile fichier = new RandomAccessFile(nomDeFichier, "rwd");
-        fichier.writeChars(contenu);
-        fichier.close();
+        buffer.flush();
+        buffer.close();
     }
 
     /**
      * retourne le nombre d'occurences du terme numéro nterm dans le document numéro ndoc.
      * @param  ndoc  le numéro du document
      * @param  nterm le numéro du terme
-     * @return       le nombre d'occurences du terme dans le document
+     * @return le nombre d'occurences du terme dans le document
      */
     public int val(int ndoc, int nterm) {
         return this.matrice[ndoc][nterm];
@@ -59,7 +101,7 @@ public class MatriceIndexNaive implements MatriceIndex {
      * affecte à val le numéro d'occurences du terme numéro nterm dans le document numéro ndoc.
      * @param  ndoc  le numéro du document
      * @param  nterm le numéro du terme
-     * @param val    la nouvelle valeur du nombre d'occurence
+     * @param  val   la nouvelle valeur du nombre d'occurence
      */
     public void affecte(int ndoc, int nterm, int val) {
 		this.matrice[ndoc][nterm] = val;
--- src/test/java/inf353/MatriceIndexNaiveTest.java
+++ src/test/java/inf353/MatriceIndexNaiveTest.java
@@ -1,15 +1,50 @@
 package inf353;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.junit.Test;
 
 public class MatriceIndexNaiveTest {
+
+    public String matriceToString(int[][] m) {
+        String matrice = "";
+        for (int i = 0; i < m[0].length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                matrice += m[i][j] + " ";
+            }
+            matrice += '\n';
+        }
+        return matrice;
+    }
     
     @Test
-    public void constructeurTest() throws IOException {
-        MatriceIndexNaive matrice1 = new MatriceIndexNaive(10, 10);
-        matrice1.sauver("test-matrice1.txt");
+    public void constructeursTest() throws IOException {
+
+        MatriceIndexNaive m1 = new MatriceIndexNaive(3,3);
+        int[][] attendu1 = {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+        };
+        assertEquals(matriceToString(m1.matrice), matriceToString(attendu1));
+
+        MatriceIndexNaive m2 = new MatriceIndexNaive("test-matrice1.txt");
+        int[][] attendu2 = {
+            {1,0,0,0,0,0,0,0,0,0},
+            {0,2,0,0,0,0,0,0,0,0},
+            {0,0,3,0,0,0,0,0,0,0},
+            {0,0,0,4,0,0,0,0,0,0},
+            {0,0,0,0,5,0,0,0,0,0},
+            {0,0,0,0,0,6,0,0,0,0},
+            {0,0,0,0,0,0,7,0,0,0},
+            {0,0,0,0,0,0,0,8,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,10}
+        };
+        assertEquals(matriceToString(m2.matrice), matriceToString(attendu2));
+        
     }
 
 }
