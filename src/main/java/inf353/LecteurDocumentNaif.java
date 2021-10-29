e16c7021e981638a927e963ccf555316bf23215e
ValuWaurld <val0u74mc@gmail.com>
2021-10-29 15:30:34 +0200

    Mise à jour de LecteurDocumentNaif

--- src/main/java/inf353/LecteurDocumentNaif.java
+++ src/main/java/inf353/LecteurDocumentNaif.java
@@ -2,50 +2,26 @@ package inf353;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.FileReader;
 
 public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
     
-<<<<<<< HEAD
     public char[] separateurs = {',', '?', '.', ';', '/', '!', ' ', '\n', '(', ')', '"', '-', '\'', '[', ']'};
     public int i;
     public String texte;
     public BufferedReader br;
 
-    public LecteurDocumentNaif(String nomFichier) throws FileNotFoundException {
+    public LecteurDocumentNaif(String nomFichier) throws IOException {
         this.texte = "";
         br = new BufferedReader(new FileReader(nomFichier));
         String ligne = br.readLine();
+        
     }
 
     public void demarrer() {
         this.i = 0;
         if (estSeparateur(texte.charAt(0))) this.avancer();
-=======
-    /**
-     * Initialisation du parcours.
-     */
-    public char[] separateurs = {',','?','.',';',':','/',' ','\n','(',')','\"','-','\'','[',']','_'}; // liste des separateurs
-    public int i; // indice du caractere dans le texte
-    public String mot = "";
-    public BufferedReader br;
-
-    public LecteurDocumentNaif(String file) throws FileNotFoundException, java.io.IOException {
-        br = new BufferedReader(new FileReader(file));
-        String strCurrentLine = "";
-        i = 0;
-        while((strCurrentLine = br.readLine()) != null)
-        {
-            mot += strCurrentLine;
-            System.out.println(strCurrentLine);
-        }
-
-    }
-
-    public void demarrer() {
-        i= 0;
->>>>>>> refs/remotes/origin/main
     }
 
     /**
@@ -89,17 +65,9 @@ public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
      */
     public boolean estSeparateur(char c) {
         int j = 0;
-<<<<<<< HEAD
         while (j < this.separateurs.length && this.separateurs[j] != c) {
             j++;
         }
         return j < this.separateurs.length;
-=======
-        while (j < separateurs.length && separateurs[j] != c)
-        {
-            j++;
-        }
-        return j < separateurs.length;
->>>>>>> refs/remotes/origin/main
     }
 }
