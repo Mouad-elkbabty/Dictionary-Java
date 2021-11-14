21e73a5f1bba00d0073fe9f514734b2576a88afd
ValuWaurld <val0u74mc@gmail.com>
2021-11-14 18:46:22 +0100

    Modification du LecteurDocumentNaif + ajout des tests

--- src/main/java/inf353/LecteurDocumentNaif.java
+++ src/main/java/inf353/LecteurDocumentNaif.java
@@ -1,79 +1,77 @@
 package inf353;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
     
-        
+	File fichier;
 	FileReader fileReader;
-    String texte;
+	char caractereLu;
     String mot;
-    int contenu;
-	char separateurs[] = {',', ';', '?', '.', '!', ':', ' ', '\t', '\n', '{', '}', '(', ')', '"', '&', '-', '_', '\'', '/','\r'};
-	int i;
+	static char[] separateurs = {',', ';', '?', '.', '!', ':', ' ', '\t', '\n', '{', '}', '(', ')', '"', '&', '-', '_', '\'', '/','\r'};
 
-
-    public LecteurDocumentNaif(String file){
-        
-		try {
-            i = 0;
-			fileReader = new FileReader(file);
-			contenu = fileReader.read();		
-		
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	/**
+	 * Construit un LecteurDocumentNaif
+	 * @param nomDuFichier le chemin vers le fichier
+	 * @throws IOException
+	 */
+    public LecteurDocumentNaif(String nomDuFichier) throws IOException {
+		this.fichier = new File(nomDuFichier);
+        if (!this.fichier.exists() || !this.fichier.isFile()) {
+            throw new FileNotFoundException("Aucun fichier du nom de " + nomDuFichier + " n'a été trouvé.");
+        }
     }
 
-    public void demarrer(){
-        try {
-				
-			if(estSeparateur((char) contenu)){
-				contenu = fileReader.read();
-			}
-
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
+	/**
+	 * Démarrer le LecteurDocumentNaif, se place sur le premier mot
+	 */
+    public void demarrer() throws IOException {
+		this.fileReader = new FileReader(this.fichier.getPath());
+		this.caractereLu = (char) this.fileReader.read();
+		this.avancer();
     }    
 	
-    public void avancer(){
-        try {
-
-        	if(estSeparateur((char) contenu)){
-            
-				contenu = fileReader.read();
-			}
-				
-			mot = "";
-			while(!estSeparateur((char) contenu)){
-				mot = mot + contenu;
-				contenu = fileReader.read();
-			}       	 
-			
-		}catch (IOException e) {
-			e.printStackTrace();
+	/**
+	 * Avance vers le prochain mot
+	 */
+    public void avancer() throws IOException {
+		while (estSeparateur(this.caractereLu)) {
+			this.caractereLu = (char) this.fileReader.read();
 		}
-		
+		mot = "";
+		while (!estSeparateur(this.caractereLu)) {
+			this.mot += this.caractereLu;
+			this.caractereLu = (char) this.fileReader.read();
+		}
     }
 
-    public boolean estSeparateur(char c){
+	/**
+	 * Renvoie vrai si c appartient à la liste des seperateurs
+	 * @param c le caractère à tester
+	 */
+    public static boolean estSeparateur(char c) {
     	int j = 0;
-    	while(j != separateurs.length && separateurs[j] != c){
+    	while (j != separateurs.length && separateurs[j] != c) {
     		j++;
     	}
     	return j != separateurs.length;
     }
 
-    public String elementCourant(){
-        return mot;
+	/**
+	 * Renvoie le mot actuellement lu
+	 */
+    public String elementCourant() {
+        return this.mot;
     }
 
+	/**
+	 * Renvoie vrai si la fin du document est atteinte
+	 */
     public boolean finDeSequence(){
-        return mot==null||mot.equals("");
+        return this.mot != null && this.mot.equals("");
     }
-
 
 }
