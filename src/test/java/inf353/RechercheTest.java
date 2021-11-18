cf79736358f0e5f548351bb207e65b88076afcb5
ValuWaurld <val0u74mc@gmail.com>
2021-11-18 19:02:24 +0100

    Ajout du fichier de tests pour Recherche

---
+++ src/test/java/inf353/RechercheTest.java
@@ -0,0 +1,25 @@
+package inf353;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class RechercheTest {
+    
+    @Test
+    public void constructeurTest() {
+
+    }
+
+    @Test
+    public void scoreTest() {
+
+    }
+
+    @Test
+    public void presentationTest() {
+
+    }
+
+}
