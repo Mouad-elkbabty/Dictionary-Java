5431a9fe598cb2f2469349998843a570e9e1b47d
fabien.vial@etu.univ-grenoble-alpes.fr <fabien.vial@etu.univ-grenoble-alpes.fr>
2021-12-02 18:39:45 +0100

    r

--- src/main/java/inf353/Recherche.java
+++ src/main/java/inf353/Recherche.java
@@ -1,20 +1,33 @@
 package inf353;
 
+import java.io.IOException;
+
 public class Recherche {
 
+    String[] recherche;
+    Indexation index;
     /**
      * Crée une Recherche
      * @param requete La requête à faire
      * @param chemin Le chemin vers l'Indexation à utiliser
      */
-    public Recherche(String[] requete, String chemin) {
-
+    public Recherche(String[] requete, String chemin) throws IOException{
+        recherche = requete;
+        index = new Indexation(chemin);
     }
 
     /**
-     * Calcule le score des documents en fonction de l'Indexation
+     * Calcule le score des documents en fonction de l'Indexation et de la recherche
      */
     public void score() {
+        int[] valeurs = new int[this.index.dictioDocuments.nbMots()];
+        int i = 0;
+        while(i < this.index.dictioDocuments.nbMots())
+        {
+            
+        }
+
+
         return;
     }
 
